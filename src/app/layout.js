0325abaa9aa74b3e062d0f443fe2b7ecd6a6d779
MarioM <a20300683@live.ceti.mx>
2024-09-28 23:41:06 -0600

    adding image on the head

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,6 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
